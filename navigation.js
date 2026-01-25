fb29a8b7c924d63a15583cf036e89e54e5fb40b7
Andrew Scherer <Andrew-Scherer@users.noreply.github.com>
2026-01-25 03:31:05 -0700

    Added Envoy role page - subordinate position to High Emissary

--- navigation.js
+++ navigation.js
@@ -4,7 +4,7 @@
     const currentPage = window.location.pathname.split('/').pop() || 'index.html';
     
     // Define which pages belong to which dropdown for active state
-    const rolesPages = ['roles.html', 'becoming-king.html', 'heir.html', 'regent.html', 'claimant.html', 'steward.html', 'master-treasury.html', 'guards.html', 'coroner.html', 'executioner.html', 'warden.html', 'high-emissary.html'];
+    const rolesPages = ['roles.html', 'becoming-king.html', 'heir.html', 'regent.html', 'claimant.html', 'steward.html', 'master-treasury.html', 'guards.html', 'coroner.html', 'executioner.html', 'warden.html', 'high-emissary.html', 'envoy.html'];
     const systemsPages = ['succession.html', 'coronation.html', 'permadeath-downed.html', 'execution-mechanics.html', 'jail.html', 'how-taxes.html', 'salary-system.html', 'housing.html', 'royal-tenure.html', 'property.html', 'bounty.html', 'seat-system.html'];
     const philosophyPages = ['philosophy.html', 'britain-sole-civic.html', 'new-player-experience.html'];
     const guildsFactionsPages = ['guilds.html', 'factions.html', 'britain-factions.html', 'faction-interactions.html'];
@@ -53,6 +53,7 @@
                 <a href="coroner.html">Coroner</a>
                 <a href="executioner.html">Executioner</a>
                 <a href="high-emissary.html">High Emissary</a>
+                <a href="envoy.html">Envoy</a>
             </div>
         </div>
         <div class="nav-dropdown">
